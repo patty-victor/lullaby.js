cd432bf9c9827a773d412e39d3f3b187d4f49efd
RMSiqueira <rafaelmsiqueira91@gmail.com>
2018-01-28 00:47:07 -0200

    Initial commit
    Signed-off-by: RMSiqueira <rafaelmsiqueira91@gmail.com>

---
+++ main.js
@@ -0,0 +1,32 @@
+//Requires
+const path = require('path')
+const express = require('express')
+const ejs = require('ejs')
+
+const app = express();
+const port = 8080;
+
+//Changing default views path
+app.set('views', path.join(__dirname, '/dev/views/layouts'));
+//EJS engine
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+
+//Routes
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/gauge', function (req, res) {
+    res.render('gauge');
+});
+
+//Start server
+app.listen(port, function (err) {
+    if (err) {
+        return console.log("Error: ", err);
+    }
+
+    console.log("Server is listening on " + port);
+})
+
